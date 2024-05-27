f6cd15e54ae170c760df7a9a8bb68a436eedf607
Diya Gupta <guptadiya114@gmail.com>
2024-05-27 23:09:26 +0530

    Add files via upload

---
+++ variable_addnum.java
@@ -0,0 +1,10 @@
+public class variable_addnum {
+    public static void main(String[] args) {
+        System.out.print("Sum of these numbers is: ");
+        int a = 2;
+        int b = 5;
+        int c = 8;
+        int sum = a + b + c;
+        System.out.println(c);
+    }
+}
