dc31e66449f11713657d3fe3df7f075937b17bb7
Diya Gupta <guptadiya114@gmail.com>
2024-05-29 17:08:30 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,31 @@
+public class Operators {
+    public static void main(String[] args) {
+        //arithematic operator
+        int a = 5;
+        int b = 5 + a;
+        System.out.println(b);
+
+        int c = 5;
+        int d = 25 - c;
+        System.out.println(d);
+
+        int e = 5;
+        int f = 75 * e;
+        System.out.println(f);
+
+        //assignment operator
+        int v = 3;
+        v += 9;
+        System.out.println(v);
+
+        //comparision operator
+        System.out.println(64<75);
+
+        //logical operator
+        System.out.println(64>5 && 87>65);
+        System.out.println(65<6 || 54>23);
+
+        //bitwise operator
+        System.out.println(2&3);
+    }
+}
