0e6ed9152b30285c521613c8f64db2c318a8b35d
Diya Gupta <guptadiya114@gmail.com>
2024-06-01 20:57:43 +0530

    Add files via upload

---
+++ If_elseby_Ternary.java
@@ -0,0 +1,8 @@
+public class If_elseby_Ternary {
+    public static void main(String[] args) {
+        int number=5;
+        //Using ternary
+        String output=(number%2==0)?"Even number":"Odd number";
+        System.out.println(output);
+    }
+}
