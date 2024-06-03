c69829a2a5b2cb5c702a4442d9d50c3581bf38ea
Diya Gupta <guptadiya114@gmail.com>
2024-06-03 19:16:49 +0530

    Add files via upload

---
+++ While_loop.java
@@ -0,0 +1,20 @@
+public class While_loop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While loop ended!");
+//        while(true){
+//            System.out.println("I am in infinite while loop!!");
+//        }
+
+        // print 100 to 200 numbers using while loop
+        int a = 101;
+        while(a<=200) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
