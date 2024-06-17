ba32c8924a2c529f8346afc500ec962a13de28a6
Diya Gupta <guptadiya114@gmail.com>
2024-06-17 15:46:18 +0530

    Add files via upload

---
+++ Fibonacci_series.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibonacci_series {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int count = input.nextInt();
+        int n1=0,n2=1,n3;
+        System.out.print(n1+" "+n2);
+        for (int i=2; i<count; i++){
+            n3 = n1 + n2;
+            System.out.print(" "+n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
