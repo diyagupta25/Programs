7c4cf2b773cb3589a4e08f4f715e3ba73cb00996
Diya Gupta <guptadiya114@gmail.com>
2024-08-03 23:53:10 +0530

    Add files via upload

--- Inheritance.java
+++ Inheritance.java
@@ -1,42 +1,13 @@
-class base{
-    int x;
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        System.out.println("I am setting x now");
-        this.x = x;
-    }
-    public void printMe(){
-        System.out.println("I am a constructor");
-    }
+class Shape{
+    String color;
 }
+class Triangle extends Shape{
 
-class derived extends base{
-    int y;
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        System.out.println("I am setting y now");
-        this.y = y;
-    }
 }
-
 public class Inheritance {
     public static void main(String[] args) {
-        base b = new base();
-        b.setX(6);
-        System.out.println(b.getX());
-
-        derived d = new derived();
-        d.setX(4);
-        System.out.println(d.getX());
-        d.setY(9);
-        System.out.println(d.getY());
+        Triangle t1 = new Triangle();
+        t1.color = "red";
+        System.out.println(t1.color);
     }
 }
